f6af80df880e96a11d6c98c7078f4bdad5751e4a
Marcelo Lourenco <marcelolourenco@relevolab.com>
2021-06-15 09:03:32 -0300

    Aprendendo Handler

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React, { Component } from "react";
-import ListaDeNotas from "./componentes/ListaDeNotas";
-import FormularioCadastro from "./componentes/FormularioCadastro";
+import ListaDeNotas from "./componentes/ListaDeNotas/";
+import FormularioCadastro from "./componentes/FormularioCadastro/";
+import "./assets/App.css";
 
 class App extends Component {
+  criarNota(titulo, texto) {
+    console.log("Uma nova nota foi criada " + this.titulo + " " + this.texto);
+  }
+
   render() {
     return (
-      <section>
-        <FormularioCadastro/>
-        <ListaDeNotas/>
+      <section className="conteudo">
+        <FormularioCadastro criarNota={this.criarNota}/>
+        <ListaDeNotas />
       </section>
     );
   }
