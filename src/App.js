2b595e0bb4fc9a5f08359341e7a9b2bf22c84e92
Marcelo Lourenco <marcelolourenco@relevolab.com>
2021-06-15 15:54:32 -0300

    Projeto Finalizado

--- src/App.js
+++ src/App.js
@@ -1,18 +1,37 @@
 import React, { Component } from "react";
 import ListaDeNotas from "./componentes/ListaDeNotas/";
 import FormularioCadastro from "./componentes/FormularioCadastro/";
+import ListaDeCategorias from "./componentes/ListaDeCategorias";
+import Categorias from "./dados/Categorias";
+import ArrayDeNotas from "./dados/Notas";
 import "./assets/App.css";
+import "./assets/index.css";
 
 class App extends Component {
-  criarNota(titulo, texto) {
-    console.log("Uma nova nota foi criada " + this.titulo + " " + this.texto);
+  constructor() {
+    super();
+    this.categorias = new Categorias();
+    this.notas = new ArrayDeNotas();
   }
 
   render() {
     return (
       <section className="conteudo">
-        <FormularioCadastro criarNota={this.criarNota}/>
-        <ListaDeNotas />
+        <FormularioCadastro
+          criarNota={this.notas.adicionarNota.bind(this.notas)}
+          categorias={this.categorias}
+        />
+        <main className="conteudo-principal">
+          <ListaDeCategorias
+            adicionarCategoria={this.categorias.adicionarCategoria.bind(this.categorias)}
+            categorias={this.categorias}
+          />
+
+          <ListaDeNotas
+            apagarNota={this.notas.apagarNota.bind(this.notas)}
+            notas={this.notas}
+          />
+        </main>
       </section>
     );
   }
--- src/componentes/ListaDeNotas/ListaDeNotas.jsx
+++ src/componentes/ListaDeNotas/ListaDeNotas.jsx
@@ -3,13 +3,38 @@ import CardNota from "../CardNota/";
 import "./style.css";
 
 class ListaDeNotas extends Component {
+
+  constructor(){
+    super();
+    this.state = {notas:[]}
+    this._novasNotas = this._novasNotas.bind(this);
+  }
+
+  componentDidMount(){
+    this.props.notas.inscrever(this._novasNotas);
+  }
+
+  componentWillUnmount(){
+    this.props.notas.desinscrever(this._novasNotas);
+  }
+
+  _novasNotas(notas){
+    this.setState({...this.state, notas});
+  }
+
   render() {
     return (
       <ul className="lista-notas">
-        {Array.of("Trabalho", "Trabalho", "Estudos").map((categoria, index) => {
+        {this.state.notas.map((nota, index) => {
           return (
             <li className="lista-notas_item" key={index}>
-              <CardNota />
+              <CardNota
+                indice={index}
+                apagarNota={this.props.apagarNota}
+                titulo={nota.titulo}
+                texto={nota.texto}
+                categoria={nota.categoria}
+              />
             </li>
           );
         })}
